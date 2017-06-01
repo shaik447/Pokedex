6bfdd8e8468e60485e1968caddf3c218e406aef8
shaik mulla syed <shaik.imp@gmail.com>
2017-06-01 15:16:22 +0300

    Initial Design Finished

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by shaik mulla syed on 9/6/1438 AH.
+//  Copyright © 1438 shaik mulla syed. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon{
+    private var _name:String!
+    private var _pokemonId:Int!
+    
+    var name:String{
+        return _name
+    }
+    
+    var pokemonId:Int{
+        return _pokemonId
+    }
+    
+    init(name:String, pokemonId:Int) {
+        self._name=name
+        self._pokemonId=pokemonId        
+    }
+}
